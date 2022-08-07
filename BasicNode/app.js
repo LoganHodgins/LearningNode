041d46ac7ea9f3af908390c537146710da81350a
Logan Hodgins <loganhodginsofficial@gmail.com>
2022-08-06 23:15:50 -0500

    Added non-blocking write file

--- BasicNode/app.js
+++ BasicNode/app.js
@@ -22,7 +22,7 @@ const server = http.createServer((req, res) => {
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message);
       res.statusCode = 302;
       res.setHeader('Location', '/');
       return res.end();
